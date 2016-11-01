bcddb14f6722f9a48ce47f76164288c1d7aca73c
designer <designer@B11R106-13>
2016-11-01 15:23:23 -0700

    added classes that are mostly just class stubs... :)

---
+++ JustForYouPromotions/Models/NewUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustForYouPromotions.Models
+{
+    public class NewUserValidator
+    {
+        public static bool IsValidUser(SiteMember sm)
+        {
+            bool isValid = true;
+
+            //TODO: Do the validation for a new user here... :)
+
+            return isValid;
+        }
+    }
+}
